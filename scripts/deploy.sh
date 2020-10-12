b89bb3a7ba568fecc1149f8b33f95ed559b1f6f5
Pavlos Vinieratos <pvinis@gmail.com>
2020-10-12 16:04:12 +0200

    try context

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,12 +7,12 @@ mkdir -p build
 node scripts/prepare.js
 
 
-# upload
-aws s3 cp build/Echo.min.json s3://artsy-public/eigen/Echo.json
+# upload a public copy and a private/backup copy
+aws s3 cp build/Echo.min.json s3://artsy-public/eigen/Echo.json --acl public-read
 aws s3 cp build/Echo.min.json s3://artsy-public/eigen/Echo_$(date +%F)_$(git rev-parse --short HEAD).json
 
 
-
+#### what about staging?
 
 
 
@@ -20,3 +20,6 @@ aws s3 cp build/Echo.min.json s3://artsy-public/eigen/Echo_$(date +%F)_$(git rev
 
 		# // invalidate cloudflare
 
+
+
+## docs about cloudfront, then cloudflare
