8fcf5007ef7df534e88c6917840a764d7d234375
Pavlos Vinieratos <pvinis@gmail.com>
2020-10-17 12:26:56 +0100

    cmp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,10 +20,27 @@ fi
 mkdir -p build
 node scripts/prepare.js
 
+MIN_JSON=build/Echo.min.json
+
+NEXT_JSON=$MIN_JSON
+
+function md5_no_upd {
+  cat "$1" | perl -pe 's|,"updated_at":".+?"||' | md5
+}
+
+CURRENT_MD5=$(md5_no_upd $CURRENT_JSON)
+NEXT_MD5=$(md5_no_upd $NEXT_JSON)
+
+if [ "$CURRENT_MD5" = "$NEXT_MD5" ]; then
+  echo "No change in file besides `updated_at`. Skipping deployment."
+  exit 0
+fi
+
+
 
 # upload a public copy and a private/backup copy
-aws s3 cp build/Echo.min.json "s3://artsy-public/eigen/Echo${NAME_POSTFIX}.json" --acl public-read
-aws s3 cp build/Echo.min.json "s3://artsy-public/eigen/Echo_$(date +%F)_$(git rev-parse --short HEAD)${NAME_POSTFIX}.json"
+aws s3 cp "$MIN_JSON" "s3://artsy-public/eigen/Echo${NAME_POSTFIX}.json" --acl public-read
+aws s3 cp "$MIN_JSON" "s3://artsy-public/eigen/Echo_$(date +%F)_$(git rev-parse --short HEAD)${NAME_POSTFIX}.json"
 
 
 # add git tag
