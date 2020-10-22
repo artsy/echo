90e36fa15271866739541e3f5235f538d9e7d47c
Ash Furrow <ash@ashfurrow.com>
2020-10-22 16:21:08 -0400

    Adds schema validation.

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -2,11 +2,14 @@ const fs = require('fs')
 const envsub = require('envsub')
 const jsonminify = require('jsonminify')
 const { exit } = require('process')
+const jsonschema = require('jsonschema')
 
 const repoDir = `${__dirname}/..`
 const templateFile = `${repoDir}/Echo.json`
 const outputFile = `${repoDir}/build/Echo.json`
 const outputFileMini = `${repoDir}/build/Echo.min.json`
+
+const schema = require('../schema.json')
 
 const prepare = async () => {
   try {
@@ -23,6 +26,13 @@ const prepare = async () => {
     // validate json
     const rawdata = fs.readFileSync(outputFile)
     const json = JSON.parse(rawdata)
+    const validator = new jsonschema.Validator()
+    const validation = validator.validate(json, schema)
+
+    if (validation.errors.length) {
+      console.log({errors: validation.errors})
+      throw "Did not pass schema validation: " + JSON.stringify(validation.errors)
+    }
 
     // minify json
     const minijson = jsonminify(JSON.stringify(json))
